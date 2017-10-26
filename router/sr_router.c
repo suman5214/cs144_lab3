16662eb2162948d70936772c5f56ca593ed8199c
suman5214 <suman5214@hotmail.com>
2017-10-26 15:32:51 -0400

    c

--- router/sr_router.c
+++ router/sr_router.c
@@ -91,7 +91,7 @@ void sr_handlepacket(struct sr_instance* sr,
 
   if (is_packet_valid(packet, len)) {
     if (pktType == ethertype_arp) {
-      sr_handle_arp_packet(sr, packet, len,interface);
+      sr_handle_arp_packet(sr, packet, len, srcAddr, destAddr, interface, eHdr);
     } else if (pktType == ethertype_ip) {
       sr_handle_ip_packet(sr, packet, len, srcAddr, destAddr, interface, eHdr);
     }
@@ -235,15 +235,17 @@ void sr_send_icmp_error_packet(uint8_t type,
 }
 
 void sr_handle_arp_packet(struct sr_instance *sr,
-        uint8_t *packet ,
+        uint8_t *packet /* lent */,
         unsigned int len,
-        char *interface) {
+        uint8_t *srcAddr,
+        uint8_t *destAddr,
+        char *interface /* lent */,   
+        sr_ethernet_hdr_t *eHdr) {
 
   printf("*** -> It is an ARP packet. Print ARP header.\n");
   print_hdr_arp(packet + sizeof(sr_ethernet_hdr_t));
 
   sr_arp_hdr_t *arpHdr = (sr_arp_hdr_t *) (packet + sizeof(sr_ethernet_hdr_t));
-  sr_ethernet_hdr_t *eHdr = (sr_ethernet_hdr_t *) packet;
 
   unsigned char senderHardAddr[ETHER_ADDR_LEN], targetHardAddr[ETHER_ADDR_LEN];
   memcpy(senderHardAddr, arpHdr->ar_sha, ETHER_ADDR_LEN);
