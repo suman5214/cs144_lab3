d1b501eb618686c00bd72e2661ccc878666f84a4
suman5214 <suman5214@hotmail.com>
2017-10-26 18:41:34 -0400

    c

--- router/sr_if.c
+++ router/sr_if.c
@@ -63,25 +63,6 @@ struct sr_if* sr_get_interface(struct sr_instance* sr, const char* name)
  *
  *---------------------------------------------------------------------*/
 
-struct sr_if* sr_get_interface_given_ip(struct sr_instance* sr, uint32_t ip)
-{
-    struct sr_if* if_walker = 0;
-
-    /* -- REQUIRES -- */
-    assert(ip);
-    assert(sr);
-
-    if_walker = sr->if_list;
-
-    while(if_walker)
-    {
-       if(if_walker->ip == ip)
-        { return if_walker; }
-        if_walker = if_walker->next;
-    }
-
-    return 0;
-} /* -- sr_get_interface_given_ip -- */
 
 /*--------------------------------------------------------------------- 
  * Method: sr_add_interface(..)
