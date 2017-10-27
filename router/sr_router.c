08046f3e8de71fc96e8616c292c25bbe2bc34edb
suman5214 <suman5214@hotmail.com>
2017-10-26 23:35:28 -0400

    c

--- router/sr_router.c
+++ router/sr_router.c
@@ -234,7 +234,7 @@ void send_icmp_packet(struct sr_instance *sr,
     printf("No MAC->IP record in talbe\n");
     return;
   }
-    struct in_addr lmp_addr = longest_matching_entry->gw.s_addr;
+    struct in_addr *lmp_addr = longest_matching_entry->gw.s_addr;
     struct sr_if *iFace = sr_get_interface(sr, longest_matching_entry->interface);
     struct sr_arpentry *arp_req = sr_arpcache_lookup(&(sr->cache), lmp_addr);
     
